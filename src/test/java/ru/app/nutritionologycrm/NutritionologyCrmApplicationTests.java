db10e2fc0f1b391d62f2734fb3fdaf2d2a4ac0a7
daniil_iagodkin <tehnokot2008@gmail.com>
2025-01-10 15:05:05 +1000

    sth

---
+++ src/test/java/ru/app/nutritionologycrm/NutritionologyCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.app.nutritionologycrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionologyCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
