afd20c77319e94a5b93a239f0680c391f3cd8f29
daniil_iagodkin <tehnokot2008@gmail.com>
2025-01-10 15:04:49 +1000

    start-point

---
+++ src/main/java/ru/app/nutritionologycrm/NutritionologyCrmApplication.java
@@ -0,0 +1,13 @@
+package ru.app.nutritionologycrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionologyCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutritionologyCrmApplication.class, args);
+    }
+
+}
