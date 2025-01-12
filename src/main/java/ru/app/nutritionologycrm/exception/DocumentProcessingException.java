c2d04309c5afc8aa6fc551af5738377371e9550c
daniil_iagodkin <tehnokot2008@gmail.com>
2025-01-12 21:35:12 +1000

    exception for document processing

---
+++ src/main/java/ru/app/nutritionologycrm/exception/DocumentProcessingException.java
@@ -0,0 +1,7 @@
+package ru.app.nutritionologycrm.exception;
+
+public class DocumentProcessingException extends RuntimeException {
+    public DocumentProcessingException(String message) {
+        super(message);
+    }
+}
