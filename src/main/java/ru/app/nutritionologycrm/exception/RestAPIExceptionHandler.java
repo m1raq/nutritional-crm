4bc3e1aa354bb8ade9073fbaff3574eae8cf2983
daniil_iagodkin <tehnokot2008@gmail.com>
2025-01-12 19:37:07 +1000

    handler for APIs exceptions was added

---
+++ src/main/java/ru/app/nutritionologycrm/exception/RestAPIExceptionHandler.java
@@ -0,0 +1,20 @@
+package ru.app.nutritionologycrm.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import ru.app.nutritionologycrm.dto.ResponseMessage;
+
+@ControllerAdvice
+public class RestAPIExceptionHandler {
+
+
+    @ExceptionHandler(value = EntityProcessingException.class)
+    public ResponseEntity<ResponseMessage> handleEntityProcessingException(EntityProcessingException e) {
+        return new ResponseEntity<>(ResponseMessage.builder()
+                .success(false)
+                .message(e.getMessage())
+                .build(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
