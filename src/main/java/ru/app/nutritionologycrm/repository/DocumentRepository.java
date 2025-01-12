44ce94ea1a6422982de3f6f99b11afd65968f5d1
daniil_iagodkin <tehnokot2008@gmail.com>
2025-01-12 21:36:17 +1000

    some queries were added

--- src/main/java/ru/app/nutritionologycrm/repository/DocumentRepository.java
+++ src/main/java/ru/app/nutritionologycrm/repository/DocumentRepository.java
@@ -4,6 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.app.nutritionologycrm.entity.DocumentEntity;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface DocumentRepository extends JpaRepository<DocumentEntity, Long> {
+
+    Boolean existsByUserUsernameAndName(String username, String name);
+
+    List<DocumentEntity> findAllByUserUsername(String username);
+
+    DocumentEntity findByNameAndUserUsername(String name, String username);
+
+    List<DocumentEntity> findByDateAndUserUsername(Date date, String username);
 }
