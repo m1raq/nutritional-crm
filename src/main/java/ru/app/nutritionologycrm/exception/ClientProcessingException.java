147f303c0acbf5a81845992eaad13a09d1b664a7
daniil_iagodkin <tehnokot2008@gmail.com>
2025-01-12 14:45:36 +1000

    abstract exception for clients

---
+++ src/main/java/ru/app/nutritionologycrm/exception/ClientProcessingException.java
@@ -0,0 +1,7 @@
+package ru.app.nutritionologycrm.exception;
+
+public class ClientProcessingException extends RuntimeException {
+    public ClientProcessingException(String message) {
+        super(message);
+    }
+}
