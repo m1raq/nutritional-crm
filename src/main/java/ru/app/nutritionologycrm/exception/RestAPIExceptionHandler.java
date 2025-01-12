24341370fc44547679cce8ef33316c783e08bf10
daniil_iagodkin <tehnokot2008@gmail.com>
2025-01-12 23:38:53 +1000

    some changes

--- src/main/java/ru/app/nutritionologycrm/exception/RestAPIExceptionHandler.java
+++ src/main/java/ru/app/nutritionologycrm/exception/RestAPIExceptionHandler.java
@@ -4,15 +4,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import ru.app.nutritionologycrm.dto.ResponseMessage;
+import ru.app.nutritionologycrm.dto.ResponseMessageDTO;
 
 @ControllerAdvice
 public class RestAPIExceptionHandler {
 
 
     @ExceptionHandler(value = {EntityProcessingException.class, DocumentProcessingException.class})
-    public ResponseEntity<ResponseMessage> handleEntityProcessingException(RuntimeException e) {
-        return new ResponseEntity<>(ResponseMessage.builder()
+    public ResponseEntity<ResponseMessageDTO> handleEntityProcessingException(RuntimeException e) {
+        return new ResponseEntity<>(ResponseMessageDTO.builder()
                 .success(false)
                 .message(e.getMessage())
                 .build(), HttpStatus.INTERNAL_SERVER_ERROR);
