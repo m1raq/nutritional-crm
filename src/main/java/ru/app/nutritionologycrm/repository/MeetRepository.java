fe5e3f56c716e797e29429a55d20db63d50f2666
daniil_iagodkin <tehnokot2008@gmail.com>
2025-01-12 23:39:30 +1000

    some queries were added

--- src/main/java/ru/app/nutritionologycrm/repository/MeetRepository.java
+++ src/main/java/ru/app/nutritionologycrm/repository/MeetRepository.java
@@ -4,11 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.app.nutritionologycrm.entity.MeetEntity;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface MeetRepository extends JpaRepository<MeetEntity, Long> {
 
-    List<MeetEntity> findAllByClientContacts(String contacts);
+    List<MeetEntity> findAllByClientId(Long id);
+
+    Boolean existsByDate(Date date);
+
+    Boolean existsByDateAndId(Date date, Long id);
 
 }
--- src/main/java/ru/app/nutritionologycrm/repository/RecommendationRepository.java
+++ src/main/java/ru/app/nutritionologycrm/repository/RecommendationRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.app.nutritionologycrm.entity.RecommendationEntity;
 
+import java.util.List;
+
 @Repository
 public interface RecommendationRepository extends JpaRepository<RecommendationEntity, Long> {
+
+    List<RecommendationEntity> findAllByClientId(Long id);
+
 }
